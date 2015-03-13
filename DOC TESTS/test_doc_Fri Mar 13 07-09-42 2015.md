0531ff970a66362206d92e3195fbdd17a3036959
Podshot <ben.gothard3@gmail.com>
2015-03-13 07:13:14 -0400

    More documentation tests

---
+++ DOC TESTS/test_doc_Fri Mar 13 07-09-42 2015.md
@@ -0,0 +1,11 @@
+## Test
+
+**Undocumented**
+
+| Function name | Arguments | Description |
+| ------------- | --------- | ----------- |
+| default_args | arg, argument | 
+        Test Function
+        :param arg: arg
+        :param argument: argument
+         |
--- utilities/doc_creator.py
+++ utilities/doc_creator.py
@@ -2,6 +2,7 @@ import inspect
 import time
 from pymclevel import infiniteworld
 import os
+import test
 
 
 class NotAModule(Exception):
@@ -9,23 +10,46 @@ class NotAModule(Exception):
 
 
 
+def organizeParameters(func):
+    params = []
+    for arg in func:
+        if arg != "self":
+            params.append(arg)
+    params = ", ".join(params)
+    return params
+
+
 def create_docs():
     lines = []
-    classes = inspect.getmembers(infiniteworld, inspect.isclass)
+    classes = inspect.getmembers(test, inspect.isclass)
     for clazz in classes:
         lines.append("## "+str(clazz[0])+"\n\n")
         print clazz[1]
-        print clazz[1].__doc__
-        lines.append(str(clazz[1].__doc__)+"\n\n")
+        doc = str(clazz[1].__doc__).replace("    ", "")
+        if doc == "None":
+            doc = "**Undocumented**"
+        lines.append(doc+"\n\n")
         methods = inspect.getmembers(clazz[1], inspect.ismethod)
+        if methods != []:
+            lines.append("| Function name | Arguments | Description |\n| ------------- | --------- | ----------- |\n")
+            for method in methods:
+                print method[1].func_code.co_varnames
+                print method[1].__doc__
+                params = organizeParameters(method[1].func_code.co_varnames)
+                print params
+                print "| {0} | {1} | {2} |".format(method[0], params, method[1].__doc__)
+                lines.append("| {0} | {1} | {2} |".format(method[0], params, method[1].__doc__))
+        lines.append("\n")
         '''
         for method in methods:
             print method[1].__doc__
         '''
         
     print "======="
-    with open("DOC TESTS"+os.path.sep+"test_doc_"+str(time.ctime()).replace(":", "-")+".md", 'w') as f:
+    out = "test_doc_"+str(time.ctime()).replace(":", "-")+".md"
+    with open("DOC TESTS"+os.path.sep+out, 'w') as f:
         f.writelines(lines)
+    print "Wrote: "+str(out)
 
 
 if __name__ == "__main__":
